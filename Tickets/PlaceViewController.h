f7372e4af78d4ff24d169c6a197d720d984ffc42
a.vasilchenko <anton.miet@gmail.com>
2020-12-29 13:37:30 +0300

    Practice 2

---
+++ Tickets/PlaceViewController.h
@@ -0,0 +1,30 @@
+//
+//  PlaceViewController.h
+//  Tickets
+//
+//  Created by Антон Васильченко on 29.12.2020.
+//
+
+#import <UIKit/UIKit.h>
+#import "DataManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef enum PlaceType {
+    PlaceTypeArrival,
+    PlaceTypeDeparture
+} PlaceType;
+
+@protocol PlaceViewControllerDelegate <NSObject>
+- (void)selectPlace:(id)place withType:(PlaceType)placeType andDataType:(DataSourceType)dataType;
+@end
+
+@interface PlaceViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) id<PlaceViewControllerDelegate>delegate;
+- (instancetype)initWithType:(PlaceType)type;
+
+@end
+
+
+NS_ASSUME_NONNULL_END
